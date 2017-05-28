1d590ca9060c61323b9515671192f025443dbc2e
TriS <tris.1027@gmail.com>
2017-05-28 16:42:14 +1000

    deploy path update

---
+++ src/app/hotel-item/hotel-item.component.ts
@@ -0,0 +1,13 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-hotel-item',
+  templateUrl: './hotel-item.component.html',
+  styleUrls: ['./hotel-item.component.css']
+})
+export class HotelItemComponent {
+
+  @Input()
+  public hotel: Hotel;
+
+}
